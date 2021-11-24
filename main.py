6418009195eafa7e7a22330ae2a9c41252d0a4ed
minato-bb <s1922049@stu.musashino-u.ac.jp>
2021-11-25 01:13:34 +0900

    main.pyの実行文記述

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from Movie2impression import movie2imp, movie_frame
 from Word2vec import word2vec
 from Impression2music import main_norm
+
+#動画をフレーム毎に分けて保存
+movie_frame.split_frames()
+
+#動画から印象語を選定
+word = movie2imp.movie2impression()
+
+#Word2vec
+impression = word2vec.vector(word)
+
+#印象語から音楽を推薦
+main_norm.music_impression(impression)
